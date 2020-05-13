da1639a4bde750d90b6d396e5e4930e03fa6d8cc
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2020-05-13 10:11:29 -0700

    0.16

--- lib/HTTP/Tinyish.pm
+++ lib/HTTP/Tinyish.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Carp ();
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 our $PreferredBackend; # for tests
 our @Backends = map "HTTP::Tinyish::$_", qw( LWP HTTPTiny Curl Wget );
